fc22c78a62bb27126e7b84a584578275447cb2c8
Wellington <wellington201197@gmail.com>
2024-05-17 21:22:07 -0300

    Correção de bugs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Usuario } from './model/dao/usuario_entity.dao';
 import { UsuarioModule } from './modules/usuario.module';
 import { AuthModule } from './modules/auth.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import typeorm from './model/dao/typeorm';
+import DatabaseModule from './modules/database.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [typeorm],
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) =>
-        configService.get('typeorm'),
-    }),
     TypeOrmModule.forFeature([Usuario]),
     UsuarioModule,
     AuthModule,
+    DatabaseModule,
   ],
   exports: [TypeOrmModule],
 })
